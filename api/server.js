127b01ce72e4b0cb507fa1a784b428c895660001
LeeJnuGoo <leejungoo1396@gmail.com>
2025-01-22 16:07:26 +0900

    [Fix] 환경 변수 정상 작동 여부 확인 테스트

--- api/server.js
+++ api/server.js
@@ -3,8 +3,8 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router(process.env.DB_ROUTER || 'db.json');
-const port = process.env.SERVER_PORT || 4000;
+const router = jsonServer.router(process.env.DB_ROUTER);
+const port = process.env.SERVER_PORT;
 
 server.use(middlewares);
 
